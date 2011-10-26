aeb4aa47ec5ddb5404827464129dec745a4587c5
Andrey Sapozhnikov <andrey.askold@gmail.com>
2011-10-26 18:19:26 +0400

    check of code

--- tools/nbbuilder/main.cpp
+++ tools/nbbuilder/main.cpp
@@ -5,7 +5,17 @@ using namespace std;
 
 int main ( int argc, char **argv ) {
 
-    cout << "Hello world" << endl;
+    Step *s1 = new StepImpl("name:stepA;   ");
+    Step *s2 = new StepImpl("label:L1;   add-data: Gosznak;   ");
+
+    cout << "s1 name: " << s1->get_name() << endl;
+    cout << "s1 is label: " << s1->is_label() << endl;
+    cout << "s1 add_data: " << s1->get_add_data() << endl;
+
+    cout << "s2 name: " << s2->get_name() << endl;
+    cout << "s2 is label: " << s2->is_label() << endl;
+    cout << "s2 add_data: " << s2->get_add_data() << endl;
+
 
     return 0;
 }
