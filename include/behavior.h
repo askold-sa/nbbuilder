1318e9b5d70aea7891420185eb4db3e54707bb33
Andrey Sapozhnikov <andrey.askold@gmail.com>
2012-02-02 17:25:38 +0400

    add protoheader for behavior

---
+++ include/behavior.h
@@ -0,0 +1,23 @@
+/**
+ *  
+ * 
+ */
+ 
+ #ifndef __BEHAVIOR_H
+ #define __BEHAVIOR_H
+
+#include<vector>
+#include<string>
+#include<map>
+
+#include<utility>
+
+#include<boost/graph/graph_traits.hpp>
+#include<boost/graph/adjacency_list.hpp>
+ 
+#include<step.h>
+#include<trace.h>
+ 
+ 
+ 
+ #endif // __BEHAVIOR_H
--- tools/nbbuilder/main.cpp
+++ tools/nbbuilder/main.cpp
@@ -1,10 +1,8 @@
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/graph_traits.hpp>
-
 #include<iostream>
 
 #include<step.h>
 #include<trace.h>
+#include<behavior.h>
 #include<miscfuncs.h>
 
 using namespace std;
@@ -57,17 +55,6 @@ int main ( int argc, char **argv ) {
 	cout<<"Subtraces for labels: "<<l1<<" and "<<l2<<":\n";
 	TraceSet subtraces = subt(traces,l1,l2);
 	debugPrint(subtraces);	
-	
-    /*
-    Step *s1 = new StepImpl("dglkhgh name:stepA;   ");
-    Step *s2 = new StepImpl("jfGHFGA;label:L1;   add-data: Gosznak;   ");
-
-    cout << "s1 name: " << s1->get_name() << endl;
-    cout << "s1 is label: " << s1->is_label() << endl;
-
-    cout << "s2 name: " << s2->get_name() << endl;
-    cout << "s2 is label: " << s2->is_label() << endl;
-	*/
 
     return 0;
 }
